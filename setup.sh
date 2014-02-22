fcb42822e7741e6acd0c59776f2cc74b3f9393e9
Phil Lu <plu@zenoss.com>
2014-02-22 09:10:29 -0600

    updated for Darwin

--- setup.sh
+++ setup.sh
@@ -4,3 +4,13 @@ cd $(dirname $0)
 for file in *rc *.env *config; do
     ln -sf $(pwd)/$file ~/.$file
 done
+
+if [[ -d "$(uname -s)" ]]; then
+    find "$(pwd)/$(uname -s)" -type f |
+    while read file; do
+        ln -sf $file ~/$(basename $file)
+    done
+fi
+
+mkdir -p ~/.vim/swaps
+
